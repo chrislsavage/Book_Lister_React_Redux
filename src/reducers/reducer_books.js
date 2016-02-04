ab7e9ac8f119a1647163fafacb94391bbb99d0e8
Chris Savage <chrislsavage@gmail.com>
2016-02-04 17:11:10 -0500

    Pass action to App

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title: "javascript the good parts"},
-    {title: "Harry potter"},
-    {title: "the dark dtower."},
-    {title: "narnia"}
+    {title: "javascript the good parts", pages: 101},
+    {title: "harry potter", pages: 200},
+    {title: "the dark tower.", pages: 800},
+    {title: "narnia", pages: 306}
   ]
 }
